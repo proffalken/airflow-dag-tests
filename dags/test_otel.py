c46847f9d0d012b2d32295db587281453fdc2b38
Matthew Macdonald-Wallace <matt@doics.co>
2026-02-25 20:12:20 +0000

    get headers from env

--- dags/test_otel.py
+++ dags/test_otel.py
@@ -1,19 +1,4 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+import os
 from __future__ import annotations
 
 from contextlib import contextmanager
@@ -222,10 +207,12 @@ def task3(ti):
     instrument_requests(otel_tracer_provider)
 
     with task_root_span(ti, otel_task_tracer, otel_tracer_provider):
+        header = {"x-shared-secret": os.getenv("LAMBDA_SHARED_SECRET")}
         rn = random.randrange(0, 256)
         res = requests.get(
             "https://3jqmloorwqgmwwdoabvtcxp5pu0mqftr.lambda-url.eu-west-2.on.aws/?name=Matt&x=1",
-            timeout=25
+            timeout=25,
+            headers=header
         )
         
         logger.info(f"\n\tStatus: {res.status_code}\n\tBody: {res.text[:200]}")
